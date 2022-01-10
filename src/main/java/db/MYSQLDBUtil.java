b56d605b1728dbd192ac3812d07f7664e58db390
nanhuang093 <97426238+nanhuang093@users.noreply.github.com>
2022-01-10 01:52:02 -0500

    Add files via upload

---
+++ src/main/java/db/MYSQLDBUtil.java
@@ -0,0 +1,15 @@
+package db;
+
+public class MYSQLDBUtil {
+	private static final String INSTANCE = "jupiter.c4lrzlnczci1.us-east-2.rds.amazonaws.com";
+	private static final String PORT_NUM = "3306";
+	public static final String DB_NAME = "jupiter";
+	private static final String USERNAME = "admin";
+	private static final String PASSWORD = "Hn19930604";
+	public static final String URL = "jdbc:mysql://"
+			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
+			+ "?user=" + USERNAME + "&password=" + PASSWORD
+			+ "&autoReconnect=true&serverTimezone=UTC";
+
+
+}
